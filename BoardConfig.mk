0bbe192acb49090bc7c6f9c4238fd6614d2d77dd
hardwareadictos <hardwareadictos@gmail.com>
2019-09-07 21:44:50 +0200

    oneplus2: don't include legacy sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,7 +171,7 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_oneplus2
 TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
+#include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Shims
